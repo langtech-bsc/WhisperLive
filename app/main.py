fcda6ef207d3388769a4a4b3f84a0f0c243896ae
Marti Umbert <marti.umbert@bsc.es>
2025-08-01 11:37:34 +0200

    app/main.py: loads settings from config file, added health check endpoint, use language from config

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 import sys
 sys.path.append('../WhisperLive/examples/client')
+sys.path.append('../examples/client')
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 from launch_client_from_file import client_from_file
+import config
 
 from queue import Queue
 import threading
+# MODEL = "tiny"  # Default model, can be changed as needed
+# HOST = "renfe-whisperlive-gpu-asr" # "localhost"
+# PORT = "9090"  # Default port, can be changed as needed
+MODEL = config.settings.ASR_MODEL
+LANGUAGE = config.settings.ASR_LANGUAGE
+HOST = config.settings.ASR_SERVER
+PORT = config.settings.ASR_PORT
 
 app = FastAPI()
-MODEL = "tiny"  # Default model, can be changed as needed
+
+
+@app.get("/health")
+async def health_check():
+    return {"status": "ok", "message": "WhisperLive API is running"}
 
 @app.post("/transcribe")
 async def transcribe(file: UploadFile = File(...)):
@@ -30,7 +43,7 @@ async def transcribe(file: UploadFile = File(...)):
 
     # Run client_from_file in a separate thread so it doesn't block
     def run_client():
-        client_from_file(temp_path, model = MODEL, transcription_callback=transcription_callback)
+        client_from_file(temp_path, server_IP = HOST, port = PORT, model = MODEL, language = LANGUAGE, transcription_callback=transcription_callback)
         q.put("__END__")
 
     threading.Thread(target=run_client, daemon=True).start()
