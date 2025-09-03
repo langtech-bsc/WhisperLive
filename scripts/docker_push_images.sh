301ab660317a8f857f84d5e8f6549a09f55c4f16
Marti Umbert <marti.umbert@bsc.es>
2025-09-03 14:05:34 +0200

    scripts/docker_push_images.sh: script that tags local images and pushes them to gitlab

---
+++ scripts/docker_push_images.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function docker_login {
+    docker login registry.gitlab.bsc.es
+}
+
+function docker_logout {
+    docker logout registry.gitlab.bsc.es
+}
+
+function push_image {
+    docker tag $1 $2
+    docker push $2
+}
+
+REGISTRY=registry.gitlab.bsc.es/lang-tech-unit/innovation/renfe/mvp
+
+docker_login
+LOCAL_IMAGE=whisperlive-renfe-whisperlive-gpu-asr
+CLOUD_IMAGE=$REGISTRY/$LOCAL_IMAGE:latest
+docker rmi $CLOUD_IMAGE
+push_image $LOCAL_IMAGE $CLOUD_IMAGE
+
+LOCAL_IMAGE=whisperlive-renfe-whisperlive-gpu-fastapi
+CLOUD_IMAGE=$REGISTRY/$LOCAL_IMAGE:latest
+docker rmi $CLOUD_IMAGE
+push_image $LOCAL_IMAGE $CLOUD_IMAGE
+
+docker_logout
