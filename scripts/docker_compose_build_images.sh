12d94129a58db2b6bfefe2e9ae7fbe0e79b14edf
Marti Umbert <marti.umbert@bsc.es>
2025-09-12 13:55:34 +0200

    scripts/docker_compose_build_images.sh: use ENV_FILE to run docker compose

--- scripts/docker_compose_build_images.sh
+++ scripts/docker_compose_build_images.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
 COMPOSE_FILE="docker-compose.yml"
+ENV_FILE=".env"
 
-cmd="docker compose -f $COMPOSE_FILE up --build"
 
-echo $cmd
-eval $cmd
+# FUNCTIONS
+show_env_file(){
+    if [ -f "$ENV_FILE" ]; then
+        echo "Using environment file: $ENV_FILE"
+        echo -e "###################################\n"
+        cat "$ENV_FILE"
+        echo -e "\n#####################################"
+    else
+        echo "ERROR: Environment file $ENV_FILE does not exist. Exiting..." && exit 1
+    fi
+}
+
+docker_compose(){
+
+    cmd="docker compose --env-file $ENV_FILE -f $COMPOSE_FILE up --build"
+    echo $cmd && eval $cmd
+
+}
+
+clean_previous_images(){
+    echo -e "\nCleaning up previous images related to whisperlive...\n"
+    bash scripts/docker_clean.sh
+}
+
+# MAIN
+show_env_file
+#clean_previous_images
+docker_compose
