bb1180e1cc6cc1969e83a04e673b33912fda463d
Marti Umbert <marti.umbert@bsc.es>
2025-09-12 13:56:17 +0200

    scripts/docker_compose_pull_images.sh: use ENV_FILE to run docker compose

--- scripts/docker_compose_pull_images.sh
+++ scripts/docker_compose_pull_images.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
-COMPOSE_FILE="docker-compose-pull-images.yml"
-cmd="docker compose -f $COMPOSE_FILE up --build"
+# COMPOSE_FILE="docker-compose-pull-images-production.yml"
+# ENV_FILE=".env-production-renfe"
 
-docker login registry.gitlab.bsc.es
-echo $cmd
-eval $cmd
-docker logout registry.gitlab.bsc.es
+COMPOSE_FILE="docker-compose-pull-images.yml"
+ENV_FILE=".env"
+
+# FUNCTIONS
+show_env_file(){
+    if [ -f "$ENV_FILE" ]; then
+        echo "Using environment file: $ENV_FILE"
+        echo -e "###################################\n"
+        cat "$ENV_FILE"
+        echo -e "\n#####################################"
+    else
+        echo "ERROR: Environment file $ENV_FILE does not exist. Exiting..." && exit 1
+    fi
+}
+
+docker_compose(){
+
+    cmd="docker compose --env-file $ENV_FILE -f $COMPOSE_FILE up --build"
+    echo $cmd && eval $cmd
+
+}
+
+docker_login(){
+    docker login registry.gitlab.bsc.es
+}
+
+docker_logout(){
+    docker logout registry.gitlab.bsc.es
+}
+
+# MAIN
+docker_login
+show_env_file
+docker_compose
+docker_logout
