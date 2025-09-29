dd178ea36cffb2b6d32a068f9fa9692eb5d70b22
Marti Umbert <marti.umbert@bsc.es>
2025-09-29 14:14:35 +0200

    scripts/docker_push_images.sh: semantinc and SHA versioning

--- scripts/docker_push_images.sh
+++ scripts/docker_push_images.sh
@@ -11,13 +11,21 @@ function docker_logout {
 function push_image {
     docker tag $1 $2
     docker push $2
+    echo "Pushed image: $2"
 }
 
 REGISTRY=registry.gitlab.bsc.es/lang-tech-unit/innovation/renfe/mvp
 
 docker_login
 LOCAL_IMAGE=whisperlive-renfe-whisperlive-gpu
-CLOUD_IMAGE=$REGISTRY/$LOCAL_IMAGE:latest
+# Semantic versioning
+TAG=v1.0.0
+CLOUD_IMAGE=$REGISTRY/$LOCAL_IMAGE:$TAG
+docker rmi $CLOUD_IMAGE
+push_image $LOCAL_IMAGE $CLOUD_IMAGE
+# SHA versioning
+TAG=$(git rev-parse --short HEAD)
+CLOUD_IMAGE=$REGISTRY/$LOCAL_IMAGE:$TAG
 docker rmi $CLOUD_IMAGE
 push_image $LOCAL_IMAGE $CLOUD_IMAGE
 
