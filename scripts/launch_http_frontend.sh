bfe77fe7897e964e3deed49b5f90d2b2e37b5f7c
Martí Umbert <marti.umbert@bsc.es>
2025-10-07 15:44:54 +0200

    scripts/launch_http_frontend.sh: HTML_PORT is now a parameter and parameter validation

--- scripts/launch_http_frontend.sh
+++ scripts/launch_http_frontend.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-cd frontend
-python3 -m http.server 8081
+HTML_PORT=$1
+
+SCRIPT_NAME=$0
+SCRIPT_BASEFOLDER=$(realpath $(realpath .)/../)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FRONTEND_FOLDER=$(realpath "$SCRIPT_DIR/../frontend4")
+DELIMITER="####################"
+pwd
+[ -z $HTML_PORT ] && echo -e "$DELIMITER\n[$SCRIPT_NAME] ERROR: HTML_PORT variable not defined\nIt should be exported before running the script)\nExiting...\n$DELIMITER" && exit
+[ ! -d $FRONTEND_FOLDER ] && echo -e "ERROR: frontend folder does not exist: $FRONTEND_FOLDER. Exiting..." && exit
+
+cd $FRONTEND_FOLDER
+cmd="python3 -m http.server $HTML_PORT"
+msg=">> Launching HTML server:\n\t$cmd"
+echo -e $msg && eval $cmd
 
