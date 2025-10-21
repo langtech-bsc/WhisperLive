0723fc207061f9dbdcf1c006ff5c78c22fda00e7
Martí Umbert <marti.umbert@bsc.es>
2025-10-21 12:15:19 +0200

    scripts/launch_servers.sh: launch only ASR server

--- scripts/launch_servers.sh
+++ scripts/launch_servers.sh
@@ -15,12 +15,15 @@ validate_variable(){
 }
 
 validate_variable ASR_PORT $ASR_PORT 
-validate_variable FASTAPI_PORT $FASTAPI_PORT
+# validate_variable FASTAPI_PORT $FASTAPI_PORT
 # validate_variable HTML_PORT $HTML_PORT
 
-echo "Launching ASR server, HTML testing frontend and FastAPI..."
-echo -e "\tConfiguration: ASR_PORT=${ASR_PORT}, FASTAPI_PORT=${FASTAPI_PORT}"
+# echo "Launching ASR server, HTML testing frontend and FastAPI..."
+# echo -e "\tConfiguration: ASR_PORT=${ASR_PORT}, FASTAPI_PORT=${FASTAPI_PORT}"
 
-python3 run_server_api.py --port ${ASR_PORT} &
+# python3 run_server_api.py --port ${ASR_PORT} &
 # bash $DIR_NAME/launch_http_frontend.sh ${HTML_PORT} &
-fastapi dev main.py --host 0.0.0.0 --port ${FASTAPI_PORT}
+# fastapi dev main.py --host 0.0.0.0 --port ${FASTAPI_PORT}
+
+echo "Launching ASR server"
+python3 run_server_api.py --port ${ASR_PORT}
